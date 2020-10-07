3f86ad5bf7c731bb8db222bbc63078bde1520f48
Solo Hack <ankit.mit1234@gmail.com>
2020-10-07 17:49:57 +0530

    Add project files.

---
+++ Basics/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basics.Controllers
+{
+    public class HomeController:Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var loginClaim = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"bob"),
+                new Claim(ClaimTypes.Email,"bpb@gmail.com"),
+                new Claim("test","copy that")
+
+
+            };
+
+            var loginLicenseClaim = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"bob"),
+                new Claim(ClaimTypes.Email,"bpb@gmail.com"),
+                new Claim("license","copy that")
+
+
+            };
+
+            var loginIdentity = new ClaimsIdentity(loginClaim,"login");
+            var licenseIdentity= new ClaimsIdentity(loginLicenseClaim, "license Login");
+
+            var principalUser = new ClaimsPrincipal( new[] { loginIdentity, licenseIdentity });
+
+            HttpContext.SignInAsync(principalUser);
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ Server/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Constants
+    {
+        public const string Audiance = "http://localhost:51832";
+        public const string Issuer = Audiance;
+        public const string Secret = "It_should_be_long_not_short";
+    }
+}
